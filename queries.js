1a171f7bd8088f282754ccaeb302aa25ded191fa
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-01-19 17:46:58 -0500

    fixed module exports syntax

--- queries.js
+++ queries.js
@@ -1,23 +1,24 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
-    user: 'sebiseb',
-    host: 'localhost',
-    database: 'sebiseb',
-    password: 'Sest9777',
-    port: 5432
-})
+  user: "",
+  host: "localhost",
+  database: "",
+  password: "",
+  port: 5432,
+});
 
 const getCars = (req, res) => {
-    pool.query('select * from Car', (error, results) =>{
-        if(error){
-            throw error;
-        }else {
-            res.status(200).send(results)
-        }
-    });
-}
+  pool.query("select * from car", (error, results) => {
+    if (error) {
+      throw error;
+    } else {
+      res.status(200).send(results.rows);
+    }
+  });
+};
 
-module.export = {
-    getCars
-}
+const getOwners = () => {};
 
+module.exports = {
+  getCars,
+};
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
-
 const queries = require("./queries");
 
-app.get("/", (queries.getCars))
+app.get(express.json());
 
-app.listen(3000, (req, res) => {
-    console.log("listening right now")
-})
+app.get("/", queries.getCars);
+
+app.listen(3000, () => {
+  console.log("listening right now");
+});
