26a09d327f569d41cb1ae3f39ab1f6126e097efd
rmpolanco <ramonpolanco0205@gmail.com>
2022-01-19 19:32:01 -0500

    Updated sql file; closes issues 1/2/3/4/5/6/7

--- database.sql
+++ database.sql
@@ -2,31 +2,6 @@ CREATE DATABASE CarDataBase;
 
 \c cardatabase;
 
-CREATE TABLE Car(
-    car_id int PRIMARY KEY,
-    brand varchar(20), --FOREIGN KEY,
-    model_year int,
-    model varchar(20),
-    color varchar(20)
-);
-
-CREATE TABLE CarOwner(
-    owner_id int PRIMARY KEY,
-    car_id int, --FOREIGN KEY,
-    owner_name varchar(30),
-    dob date, 
-    phone varchar(20),
-    owner_address varchar(255)
-);
-
-CREATE TABLE Purchase(
-    purchase_id int PRIMARY KEY,
-    car_id int, --FOREIGN KEY,
-    owner_id int, --FOREIGN KEY,
-    brand varchar(20), --FOREIGN KEY,
-    cost int
-
-);
 
 CREATE TABLE Dealer(
     brand varchar(20) PRIMARY KEY,
@@ -35,9 +10,54 @@ CREATE TABLE Dealer(
     revenue bigint,
     employees int
 );
+CREATE TABLE Car(
+    car_id int PRIMARY KEY,
+    brand varchar(20),
+    model_year int,
+    model varchar(20),
+    color varchar(20),
+    FOREIGN KEY(brand) references Dealer(brand)
+);
+CREATE TABLE CarOwner(
+    owner_id int PRIMARY KEY,
+    car_id int, 
+    owner_name varchar(30),
+    dob date, 
+    phone varchar(20),
+    owner_address varchar(255),
+    FOREIGN KEY(car_id) references Car(car_id)
+);
+CREATE TABLE Purchase(
+    purchase_id int PRIMARY KEY,
+    car_id int, 
+    owner_id int, 
+    brand varchar(20), 
+    cost int,
+    FOREIGN KEY(car_id) references Car(car_id),
+    FOREIGN KEY(owner_id) references CarOwner(owner_id),
+    FOREIGN KEY(brand) references Dealer(brand)
+);
 
-insert into Car values(1, 'Nissan', 1999, 'Altima', 'Blue'),
-(2, 'Toyota', 2012, 'Camry', 'Red');
+insert into Dealer values('Nissan','New York',10000000,100);
 
-insert into CarOwner values(1, 1, 'Jerry'),
-(2, 2, 'Tom');
+insert into Car values(1, 'Nissan', 1999, 'Altima', 'Blue');
+
+insert into CarOwner values(1, 1, 'Jerry');
+
+insert into Purchase values(1, 1, 1, 'Nissan', 10000);
+
+
+select * from Car 
+    inner join CarOwner on CarOwner.car_id = Car.car_id
+where Car.car_id = 1;
+
+select * from Car 
+    inner join Dealer on Dealer.brand = Car.brand
+    inner join CarOwner on CarOwner.car_id = Car.car_id
+where Car.car_id = 1;
+
+select * from Car 
+    inner join Dealer on Dealer.brand = Car.brand
+    inner join CarOwner on CarOwner.car_id = Car.car_id
+    inner join Purchase on Purchase.car_id = Car.car_id
+where Car.car_id = 1;
