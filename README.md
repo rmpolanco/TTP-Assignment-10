76df82df0fe7c541f546e8410c031ec0eaeafca7
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-01-19 21:43:30 -0500

    add queries, update readme images. Closes #8. Closes #9

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
+# Assignment 10 - Database Practice
+
+## FIGMA ER TABLE
 
 [Our figma outline](https://www.figma.com/file/LvsbqvPu7lrJOg37iN6aIO/TTP-Assignment-10?node-id=0%3A1)
+
+![Get Cars](./images/figma-er-diagram.png)
+
+<br/>
+
+### POSTMAN - Retrieving Data - Cars
+
+![Get Cars](./images/postman-get-cars.png)
+
+<br/>
+
+### POSTMAN - Retrieving Data - Four Table Join (Cars, Owners, Dealers, Purchases)
+
+![Get Cars, Owners, Dealers, Purchases](images/postman-get-four-table-join.png)
+
+<br/>
+
+### POSTMAN - Create Data - Dealer
+
+![New Dealer](images/postman-create-new-dealer-data.png)
--- database.sql
+++ database.sql
@@ -1,63 +1,69 @@
 CREATE DATABASE CarDataBase;
 
-\c cardatabase;
-
+\c cardatabase -- psql change to database
 
 CREATE TABLE Dealer(
-    brand varchar(20) PRIMARY KEY,
-    city varchar(255), 
-    car_type varchar(20), 
-    revenue bigint,
-    employees int
+    brand VARCHAR(20) PRIMARY KEY,
+    city VARCHAR(255), 
+    car_type VARCHAR(20), 
+    revenue BIGINT,
+    employees INTEGER
 );
+
 CREATE TABLE Car(
-    car_id int PRIMARY KEY,
-    brand varchar(20),
-    model_year int,
-    model varchar(20),
-    color varchar(20),
-    FOREIGN KEY(brand) references Dealer(brand)
+    car_id INTEGER PRIMARY KEY,
+    brand VARCHAR(20),
+    model_year INTEGER,
+    model VARCHAR(20),
+    color VARCHAR(20),
+    FOREIGN KEY(brand) REFERENCES Dealer(brand)
 );
+
 CREATE TABLE CarOwner(
-    owner_id int PRIMARY KEY,
-    car_id int, 
-    owner_name varchar(30),
-    dob date, 
-    phone varchar(20),
-    owner_address varchar(255),
-    FOREIGN KEY(car_id) references Car(car_id)
+    owner_id INTEGER PRIMARY KEY,
+    car_id INTEGER, 
+    owner_name VARCHAR(30),
+    phone VARCHAR(20),
+    owner_address VARCHAR(255),
+    FOREIGN KEY(car_id) REFERENCES Car(car_id)
 );
+
 CREATE TABLE Purchase(
-    purchase_id int PRIMARY KEY,
-    car_id int, 
-    owner_id int, 
-    brand varchar(20), 
-    cost int,
-    FOREIGN KEY(car_id) references Car(car_id),
-    FOREIGN KEY(owner_id) references CarOwner(owner_id),
-    FOREIGN KEY(brand) references Dealer(brand)
+    purchase_id INTEGER PRIMARY KEY,
+    car_id INTEGER, 
+    owner_id INTEGER, 
+    brand VARCHAR(20), 
+    cost INTEGER,
+    FOREIGN KEY(car_id) REFERENCES Car(car_id),
+    FOREIGN KEY(owner_id) REFERENCES CarOwner(owner_id),
+    FOREIGN KEY(brand) REFERENCES Dealer(brand)
 );
 
-insert into Dealer values('Nissan','New York',10000000,100);
+INSERT INTO Dealer VALUES('Nissan', 'New York', 'sedan', 10000000, 100);
+INSERT INTO Dealer VALUES('Honda', 'New York', 'SUV', 1000000000, 500);
+INSERT INTO Dealer VALUES('Mazda', 'California', 'sedan', 6432456112, 1000);
 
-insert into Car values(1, 'Nissan', 1999, 'Altima', 'Blue');
+INSERT INTO Car VALUES(1, 'Nissan', 1999, 'Altima', 'Blue');
+INSERT INTO Car VALUES(2, 'Honda', 2005, 'Accord', 'Red');
+INSERT INTO Car VALUES(3, 'Mazda', 2021, 'crv', 'black');
 
-insert into CarOwner values(1, 1, 'Jerry');
+INSERT INTO CarOwner VALUES(1, 1, 'Jerry', '555-555-5555', 'chelsea');
+INSERT INTO CarOwner VALUES(2, 2, 'Bob', '123-456-7890', 'st marks');
+INSERT INTO CarOwner VALUES(3, 3, 'Gunther', '777-777-8888', 'brooklyn');
 
-insert into Purchase values(1, 1, 1, 'Nissan', 10000);
+INSERT INTO Purchase VALUES(1, 1, 1, 'Nissan', 10000);
+INSERT INTO Purchase VALUES(2, 2, 2, 'Honda', 23434);
+INSERT INTO Purchase VALUES(3, 3, 3, 'Mazda', 44532);
 
 
-select * from Car 
-    inner join CarOwner on CarOwner.car_id = Car.car_id
-where Car.car_id = 1;
+SELECT * FROM Car 
+    INNER JOIN CarOwner ON CarOwner.car_id = Car.car_id;
 
-select * from Car 
-    inner join Dealer on Dealer.brand = Car.brand
-    inner join CarOwner on CarOwner.car_id = Car.car_id
-where Car.car_id = 1;
+SELECT * FROM Car 
+    INNER JOIN Dealer ON Dealer.brand = Car.brand
+    INNER JOIN CarOwner ON CarOwner.car_id = Car.car_id;
 
-select * from Car 
-    inner join Dealer on Dealer.brand = Car.brand
-    inner join CarOwner on CarOwner.car_id = Car.car_id
-    inner join Purchase on Purchase.car_id = Car.car_id
-where Car.car_id = 1;
+SELECT * FROM Car 
+    INNER JOIN Dealer ON Dealer.brand = Car.brand
+    INNER JOIN CarOwner ON CarOwner.car_id = Car.car_id
+    INNER JOIN Purchase ON Purchase.car_id = Car.car_id;
